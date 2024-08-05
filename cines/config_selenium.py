1265c361f2dcb296d65a6aef0d042eba5151b8ed
Diego Valencia <ve18007@ues.edu.sv>
2024-08-05 08:41:31 -0600

    Se agrego al archivo de configuraciones el método para guardar datos en xlsx.

--- cines/config_selenium.py
+++ cines/config_selenium.py
@@ -27,3 +27,30 @@ service = Service(executable_path=path)
 
 # Crear el driver de Edge con las opciones y el servicio configurados
 driver = webdriver.Edge(service=service, options=options)
+
+def write_movie_data(sheet, date, country, cinema_brand, cinema_location, movie_title, time, classification, language):
+    next_row = sheet.max_row + 1
+    sheet.cell(row=next_row, column=1).value = date
+    sheet.cell(row=next_row, column=2).value = country
+    sheet.cell(row=next_row, column=3).value = cinema_brand
+    sheet.cell(row=next_row, column=4).value = cinema_location
+    sheet.cell(row=next_row, column=5).value = movie_title
+    sheet.cell(row=next_row, column=6).value = time
+    sheet.cell(row=next_row, column=7).value = classification
+    sheet.cell(row=next_row, column=8).value = language
+
+
+# Crear libro de trabajo, hoja y fila de encabezados
+workbook = openpyxl.Workbook()
+sheet = workbook.active
+
+next_row = 1
+
+sheet.cell(row=next_row, column=1).value = "Fecha"
+sheet.cell(row=next_row, column=2).value = "Pais"
+sheet.cell(row=next_row, column=3).value = "Cine"
+sheet.cell(row=next_row, column=4).value = "Nombre Cine"
+sheet.cell(row=next_row, column=5).value = "Titulo"
+sheet.cell(row=next_row, column=6).value = "Hora"
+sheet.cell(row=next_row, column=7).value = "Formato"
+sheet.cell(row=next_row, column=8).value = "Idioma"
--- cines/novacinemas.py
+++ cines/novacinemas.py
@@ -16,35 +16,6 @@ list_all = []
 cartelera = driver.find_element(By.XPATH, '//*[@id="menu-item-103"]/a')
 cartelera.click()
 
-
-def write_movie_data(sheet, date, country, cinema_brand, cinema_location, movie_title, time, classification, language):
-    next_row = sheet.max_row + 1
-    sheet.cell(row=next_row, column=1).value = date
-    sheet.cell(row=next_row, column=2).value = country
-    sheet.cell(row=next_row, column=3).value = cinema_brand
-    sheet.cell(row=next_row, column=4).value = cinema_location
-    sheet.cell(row=next_row, column=5).value = movie_title
-    sheet.cell(row=next_row, column=6).value = time
-    sheet.cell(row=next_row, column=7).value = classification
-    sheet.cell(row=next_row, column=8).value = language
-
-
-# Crear libro de trabajo, hoja y fila de encabezados
-workbook = openpyxl.Workbook()
-sheet = workbook.active
-
-next_row = 1
-
-sheet.cell(row=next_row, column=1).value = "Fecha"
-sheet.cell(row=next_row, column=2).value = "Pais"
-sheet.cell(row=next_row, column=3).value = "Cine"
-sheet.cell(row=next_row, column=4).value = "Nombre Cine"
-sheet.cell(row=next_row, column=5).value = "Titulo"
-sheet.cell(row=next_row, column=6).value = "Hora"
-sheet.cell(row=next_row, column=7).value = "Formato"
-sheet.cell(row=next_row, column=8).value = "Idioma"
-
-
 def waitingPage():
     # Esperar a que la página se cargue completamente (ajustar según necesidad)
     WebDriverWait(driver, 10).until(
@@ -193,7 +164,6 @@ def add_data_to_excel(list):
                          list[i][3], list[i][4], list[i][5], list[i][6], list[i][7])
 
     list_all.clear()
-
 
 for i in range(3):
     display_all_cines()
