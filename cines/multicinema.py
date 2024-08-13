8650eb465d4a74e0d15fbb2c0a01e8519338c35f
menendezisma <mauriciomenendez01@gmail.com>
2024-08-12 21:15:04 -0600

    multicinemas 100%

--- cines/multicinema.py
+++ cines/multicinema.py
@@ -3,22 +3,13 @@ from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
 import time
-import locale
 import pandas as pd
 from datetime import datetime
 
-#funcion que servira para convertir la fecha al formato de mes-dia-año.
-def date_conversion(date_movies):
-    locale.setlocale(locale.LC_TIME, 'Spanish_Spain.1252')
-    date_text=date_movies.text[22:]
-    date_text=date_text.split()
-    if(date_text[0]=="Miercoles"):
-        date_text[0]="Miércoles"
-    elif date_text[0]=="Sabado":
-        date_text[0]="Sábado"
-    date_text=" ".join(date_text)
-    fecha_datetime=datetime.strptime(date_text, "%A %d %B %Y")
-    fecha_formato=fecha_datetime.strftime("%m-%d-%Y")
+#funcion que sirve para obtener la fecha actual.
+def date_conversion():
+    fecha=datetime.now()
+    fecha_formato=fecha.strftime("%m-%d-%Y")
     return fecha_formato
 
 #función que estructura los datos
@@ -75,7 +66,7 @@ def cinema_data_analyzer(data, date, country):
             #Extrae la duración
             duracion = line.split("Duración:")[1].strip()
         #Detecta la línea con el idioma y formato
-        elif line.startswith("Español"):
+        elif line.startswith("Español") or line.startswith("Subtitulada"):
             #Si hay horarios almacenados, añade las filas correspondientes a data_rows
             if horarios:
                 for hora in horarios:
@@ -146,13 +137,10 @@ def combine_time_parts(data_rows):
     return combined_data
 
 
-if __name__=='__main__':
-    #Crear instancia de Options
-    options = Options()
-    #Abrir la ventana del navegador en tamaño grande.
-    options.add_argument('--start-maximized')
-    #options.add_argument()
 
+if __name__=='__main__':
+    options = Options()
+    options.add_argument('--start-maximized')
     driver_path = 'C:\\Users\\Usuario\\Desktop\\EdgeDriver\\msedgedriver.exe'
     service=Service(driver_path)
     driver = webdriver.Edge(service=service, options=options)
@@ -162,38 +150,7 @@ if __name__=='__main__':
     cartelera_button = driver.find_element(By.LINK_TEXT, "Cartelera")
     #Dar clic al botón "Cartelera" del navbar
     cartelera_button.click()
-
-    #en la nueva página, en el selector elegimos "sabado 27 Julio 2024"
-    # Encontrar el elemento select con el nombre "Dias"
-    #select_element = driver.find_element(By.NAME, "Dias")
-
-    # Crear un objeto Select para interactuar con el elemento
-    #select_object = Select(select_element)
-
-    # Seleccionar la opción por su texto visible
-    #select_object.select_by_visible_text("Sabado 27  Julio  2024 ")
-
-    #selector1 = driver.find_element(By.XPATH, '//*[@id="frmCartelera"]/div/center/select[1]/option[2]')
-    #selector1.click()
-    #selector2 = driver.find_element(By.XPATH, '//*[@id="frmCartelera"]/div/center/select[2]/option[2]')
-    #selector2.click()
-    #tambien seleccionamos Multicinema Plaza mundo soyapango
-    #select_element2 = driver.find_element(By.NAME, "Complejo")
-    #select_object2 = Select(select_element2)
-    #select_object2.select_by_visible_text("Multicinema Plaza Mundo Soyapango")
-
-    #Dar click al boton de consulta
-    # Encontrar el botón "Consulta"
-    #consulta_button = driver.find_element(By.XPATH, "//input[@type='submit' and @value='Consulta']")
-    # Hacer clic en el botón
-    #consulta_button.click()
     time.sleep(2)
-    #name_cinema=driver.find_element(By.XPATH, '//h3[@class="panel-title"]')
-    #print(name_cinema.text[9:])
-    # Cierra el navegador
-
-    #obtener la fecha de la cartelera
-    date_cinema=driver.find_element(By.XPATH, '/html/body/div[1]/div/div/h3/center')
 
     #variables para almacenar información
     #variable estatica, ya que el cine está ubicado en el mismo pais.
@@ -208,11 +165,9 @@ if __name__=='__main__':
         #print(infos.text)
     #unir los datos como una cadena, especificandoles un salto de linea.    
     data_info = "\n".join(data_info)
-    #print(data_info)    
-    #pasar los datos
 
     #convertir la fecha que se obtiene del sitio web, al formato mes-dia-año
-    date_format=date_conversion(date_cinema)
+    date_format=date_conversion()
     #estructurar los datos en filas.
     data_rows = cinema_data_analyzer(data_info, date_format, country)
     data_rows = combine_time_parts(data_rows)
@@ -221,8 +176,9 @@ if __name__=='__main__':
     #df=df.to_string(index=False)
     print(df)
     #exportar el dataframe como un archivo de excel.
-    df.to_excel('Multicinema2.xlsx', index=False)
+    df.to_excel('Multicinema.xlsx', index=False)
     driver.quit()
+
 
 
 
