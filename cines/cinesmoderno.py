07ce946db6de6c2e272e46dbe0cf5eed821dc79e
wizard503 <josemdg12@gmail.com>
2024-08-13 09:20:31 -0600

    cines moderno 100%

--- cines/cinesmoderno.py
+++ cines/cinesmoderno.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from config_selenium import *
 
+# Obtener la fecha actual
+current_date_ = datetime.date.today().strftime('%d-%m-%Y')
 # Abrir la página web
 url='https://www.cinesmodernopanama.com/'
 driver.get(url)
@@ -21,39 +23,46 @@ elemento_menu = driver.find_element(By.XPATH, '//*[@id="cd-lateral-nav"]/ul[1]/l
 elemento_menu.click()
 
 sub_menu=wait.until(EC.visibility_of_element_located((By.XPATH,'//nav[@id="cd-lateral-nav"]//ul[@class="sub-menu"]')))
-
 cines=sub_menu.find_elements(By.XPATH,'//a[contains(@id,"Semana_")]')
-print(len(cines))
-for option in cines:
-    print(10*'-')
-    option.click()
-    combocomplejo = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="cartelera"]/div/div[1]/div'))
-    )
-    NameCine=combocomplejo.find_element(By.CSS_SELECTOR,'.subtitulocomplejo').text
-    print(NameCine)
-    peliculas=combocomplejo.find_elements(By.XPATH,'//*[@id="cartelera"]/div/div[5]/div')
-    for movie in peliculas:
-        #print(movie.get_attribute('outerHTML'))
-        print(10*'-')
-        contenedorTitulo=movie.find_element(By.CSS_SELECTOR,'.combopelicartelera .combopelititulo')
-        title=contenedorTitulo.find_element(By.TAG_NAME,'h2').text
-        
-        contenedorInfo=movie.find_element(By.CSS_SELECTOR,'.combodetallepeli .infopeli')
-        language=contenedorInfo.find_element(By.CSS_SELECTOR,'.icosdetalle p').text
-        
-        print(f'Titulo: {title} Idioma: {language}')
-        horarios_container = movie.find_element(By.CSS_SELECTOR, '.conjuntohorarios')
-        horarios = horarios_container.find_elements(By.CSS_SELECTOR, '.horarioscartelera li.func-horario')
+cines_links = [cine.get_attribute('href') for cine in sub_menu.find_elements(By.XPATH, '//a[contains(@id,"Semana_")]')]
 
-        for time in horarios:
-            link_element = time.find_element(By.TAG_NAME, 'a')
-            link_text = link_element.text
-            try:
-                hora, formato = link_text.split(' ')
-                formato = formato.strip('()')
-                print('Hora:', hora)  # Ejemplo de salida: 17:15
-                print('Formato:', formato)  # Ejemplo de salida: 2D
-            except ValueError:
-                print("Error al dividir el texto:", link_text)
+for cine_link in cines_links:
+    driver.get(cine_link)
+
+    try:
+        combocomplejo = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="cartelera"]/div/div[1]/div'))
+        )
+
+        NameCine=combocomplejo.find_element(By.CSS_SELECTOR,'.subtitulocomplejo').text
+        NameCine = NameCine.replace("En ", "")
+        peliculas=combocomplejo.find_elements(By.XPATH,'//*[@id="cartelera"]/div/div[5]/div')
+
+        for movie in peliculas:
+            contenedorTitulo=movie.find_element(By.CSS_SELECTOR,'.combopelicartelera .combopelititulo')
+            title=contenedorTitulo.find_element(By.TAG_NAME,'h2').text.capitalize()
+            title=title.replace("2d", "").replace("dob", "").strip()
+            
+            contenedorInfo=movie.find_element(By.CSS_SELECTOR,'.combodetallepeli .infopeli')
+            language=contenedorInfo.find_element(By.CSS_SELECTOR,'.icosdetalle p').text
+            
+            horarios_container = movie.find_element(By.CSS_SELECTOR, '.conjuntohorarios')
+            horarios = horarios_container.find_elements(By.CSS_SELECTOR, '.horarioscartelera li.func-horario')
+
+            for time in horarios:
+                link_element = time.find_element(By.TAG_NAME, 'a')
+                link_text = link_element.text
+                try:
+                    hora, formato = link_text.split(' ')
+                    formato = formato.strip('()')
+
+                    write_movie_data(sheet, current_date_, 'Panama', 'Cines Moderno', NameCine, title, hora, formato, language)
+                except ValueError:
+                    print("Error al dividir el texto:", link_text)
+    except:
+        print("No hay funciones disponibles")
+        continue
+
+# Guardar el libro de trabajo de Excel
+save_excel("cines_moderno",current_date_)
 driver.quit()
